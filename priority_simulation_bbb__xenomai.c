7265fd027f35e804c001615243bf463b762d48c9
Humberto Beltrão <40904989+humbertobeltrao@users.noreply.github.com>
2023-11-07 21:28:30 -0300

    fixing code

--- priority_simulation_bbb__xenomai.c
+++ priority_simulation_bbb__xenomai.c
@@ -1,42 +1,21 @@
-#include <alchemy/task.h>
 #include <stdio.h>
+#include <signal.h>
+#include <alchemy/task.h>
+#include <alchemy/sem.h>
+#include <alchemy/timer.h>
+#include <stdlib.h>
 
-void led_blink_high_prio(void *arg) 
-{
-	 int gpio_pin = 44; // GPIO number for physical pin P9_12 (GPIO_60)
-    int led_state = 0;
+#define NTASKS 2
+#define HIGH 52 /* high priority */
+#define LOW 50  /* low priority */
 
-    // Export the GPIO pin
-    char gpio_export_command[64];
-    snprintf(gpio_export_command, sizeof(gpio_export_command), "echo %d > /sys/class/gpio/export", gpio_pin);
-    system(gpio_export_command);
+RT_TASK demo_task[NTASKS];
+RT_SEM mysync;
 
-    // Set the GPIO direction to out
-    char gpio_direction_command[64];
-    snprintf(gpio_direction_command, sizeof(gpio_direction_command), "echo out > /sys/class/gpio/gpio%d/direction", gpio_pin);
-    system(gpio_direction_command);
+#define BLINK_PERIOD 500000000 // Blink period in nanoseconds (0.5 seconds)
 
-    while (1) {
-        // Toggle the LED state
-        led_state = !led_state;
-        // Set the GPIO pin value
-        char gpio_value_command[64];
-        snprintf(gpio_value_command, sizeof(gpio_value_command), "echo %d > /sys/class/gpio/gpio%d/value", led_state, gpio_pin);
-        system(gpio_value_command);
-
-        rt_task_sleep(1000000000); // Sleep for 1 second
-    }
-
-    // Unexport the GPIO pin (cleanup)
-    char gpio_unexport_command[64];
-    snprintf(gpio_unexport_command, sizeof(gpio_unexport_command), "echo %d > /sys/class/gpio/unexport", gpio_pin);
-    system(gpio_unexport_command);
-}
-
-void led_blink_low_prio(void *arg)
-{
- int gpio_pin = 46; // GPIO number for physical pin P9_12 (GPIO_60)
-    int led_state = 0;
+void controlLED(int gpio_pin) {
+   int led_state = 0;
 
     // Export the GPIO pin
     char gpio_export_command[64];
@@ -56,44 +35,54 @@ void led_blink_low_prio(void *arg)
         snprintf(gpio_value_command, sizeof(gpio_value_command), "echo %d > /sys/class/gpio/gpio%d/value", led_state, gpio_pin);
         system(gpio_value_command);
 
-        rt_task_sleep(1000000000); // Sleep for 1 second
+
+        // Sleep for the remaining half period
+        rt_task_sleep(BLINK_PERIOD / 2);
     }
 
-    // Unexport the GPIO pin (cleanup)
+     // Unexport the GPIO pin (cleanup)
     char gpio_unexport_command[64];
     snprintf(gpio_unexport_command, sizeof(gpio_unexport_command), "echo %d > /sys/class/gpio/unexport", gpio_pin);
     system(gpio_unexport_command);
 }
 
-int main(int argc, char *argv[])
-{
-	RT_TASK task_high_prio, task_low_prio;
+void demo(void *arg) {
+    int num = *(int *)arg;
+    printf("Task: %d\n", num);
+    rt_sem_p(&mysync, TM_INFINITE);
+    printf("Blinking LED %d\n", num);
 
-    // Initialize Xenomai
-
-    if (rt_task_create(&task_high_prio, "LED_Blink_High_Prio", 0, 90, T_JOINABLE) != 0) {
-        printf("Error creating the high-priority task\n");
-        return -1;
+    if (num == 0) {
+        // Control P8_12 (GPIO 44)
+        controlLED(44);
+    } else {
+        // Control P8_16 (GPIO 46)
+        controlLED(46);
     }
 
-    if (rt_task_start(&task_high_prio, &led_blink_high_prio, NULL) != 0) {
-        printf("Error starting the high-priority task\n");
-        return -1;
+    printf("End Task: %d\n", num);
+}
+
+void startup() {
+    int i;
+    char str[20];
+
+    // Semaphore to sync task startup on
+    rt_sem_create(&mysync, "MySemaphore", 0, S_FIFO);
+
+    for (i = 0; i < NTASKS; i++) {
+        printf("Start Task: %d\n", i);
+        sprintf(str, "task%d", i);
+        rt_task_create(&demo_task[i], str, 0, (i == 0) ? LOW : HIGH, 0);
+        rt_task_start(&demo_task[i], &demo, &i);
     }
 
-    if (rt_task_create(&task_low_prio, "LED_Blink_Low_Prio", 0, 70, T_JOINABLE) != 0) {
-        printf("Error creating the low-priority task\n");
-        return -1;
-    }
+    printf("Wake up all tasks\n");
+    rt_sem_broadcast(&mysync);
+}
 
-    if (rt_task_start(&task_low_prio, &led_blink_low_prio, NULL) != 0) {
-        printf("Error starting the low-priority task\n");
-        return -1;
-    }
-
-    // Wait for the tasks to finish (won't happen in this example)
-    rt_task_join(&task_high_prio);
-    rt_task_join(&task_low_prio);
-
-    return 0;
+int main(int argc, char* argv[]) {
+    startup();
+    printf("\nType CTRL-C to end this program\n\n");
+    pause();
 }
